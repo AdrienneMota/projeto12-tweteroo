6ac7b212e0730b9669b7463a94bc08fd2b77ed15
AdrienneMota <adriennemota97@gmail.com>
2022-11-05 23:18:33 -0400

    get done

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-import express from "express";
+import express, { json } from "express";
+import cors from "cors"
+import users from "./users.js";
+import tweets from "./tweets.js";
 
 const app = express()
 
+//configurations
+app.use(cors())//evita um erro de segurança
+app.use(express.json())//permite o recebimento de dados do body como json
+
+//criacao de rotas
+app.get("/tweets", (req, res) => {
+    const newtweets = []
+    const lasttweets = []
+
+    for(let i = tweets.length - 1; i> tweets.length-11; i--){
+        lasttweets.push(tweets[i])
+    }
+    
+    for (const lasttweet of lasttweets) {
+       const newtweet = {
+        username: lasttweet.username,
+        avatar: users.find((u) => u.username === lasttweet.username).avatar,
+        tweet: lasttweet.tweet,
+       }
+       newtweets.push(newtweet)
+    }
+
+    
+
+    res.send(newtweets)
+})
+
+//selecionando a porta
 app.listen('5000', () => console.log('Server is running in port: 5000'))
---
+++ tweets.js
@@ -0,0 +1,59 @@
+const tweets = [
+    {   
+        //id_user: 1,
+        username: "bobesponja",
+        tweet: "meu bb"
+    },
+    {   
+        //id_user: 1,
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+       // id_user:2,
+        username: "yor",
+        tweet: "não sei cozinhar"
+    },
+    {   
+        //id_user: 1,
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {   
+        //id_user: 1,
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {   
+        //id_user: 1,
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {   
+        //id_user: 1,
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+       // id_user:2,
+        username: "yor",
+        tweet: "não sei cozinhar"
+    },
+    {   
+        //id_user: 1,
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {   
+        //id_user: 1,
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {   
+        //id_user: 1,
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    }
+]
+
+export default tweets
